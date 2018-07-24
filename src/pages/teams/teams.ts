e03b4e3c8b1149a6cd9b563a0cf145ab5be4c9e3
x5778ab <alvaro.belmontep@redsys.es>
2018-07-24 14:46:16 +0200

    añadidas paginas

---
+++ src/pages/teams/teams.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { TeamDetailPage } from '../team-detail/team-detail';
+
+/**
+ * Generated class for the TeamsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-teams',
+  templateUrl: 'teams.html',
+})
+export class TeamsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TeamsPage');
+  }
+
+  itemTapped(){
+    this.navCtrl.push(TeamDetailPage);
+  }
+}
